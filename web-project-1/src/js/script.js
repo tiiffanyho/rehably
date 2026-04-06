4e4e6e80340038f8891233c12253e1e3ab294a5b
tiiffanyho <Tiffanyho0903@gmail.com>
2026-04-05 20:14:43 -0400

    organize and change formatting

--- web-project-1/src/js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const startButton = document.getElementById('start-now');
-
-    startButton.addEventListener('click', function() {
-        alert('Thank you for starting your journey with us!');
-        // Here you can add functionality to redirect to another page or section
-    });
-});
